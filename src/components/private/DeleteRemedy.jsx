0fdaae4b5de4959fe981d2eb1ac311575c174bad
unika1 <unikaprj@gmail.com>
2025-02-27 19:14:16 +0545

    changes

--- src/components/private/DeleteRemedy.jsx
+++ src/components/private/DeleteRemedy.jsx
@@ -22,13 +22,13 @@ function DeleteRemedy({ remedyId, onDeleteSuccess }) {
             console.log("Server response:", response);
 
             if (response.status === 200) {
-                alert("✅ Remedy deleted successfully");
+                alert("Remedy deleted successfully");
                 onDeleteSuccess(remedyId);
             } else {
                 alert("⚠ Failed to delete remedy.");
             }
         } catch (error) {
-            console.error("❌ Error deleting remedy:", error.response?.data || error.message);
+            console.error("Error deleting remedy:", error.response?.data || error.message);
             alert("❌ " + (error.response?.data?.error || "Unknown error"));
         }
     };
